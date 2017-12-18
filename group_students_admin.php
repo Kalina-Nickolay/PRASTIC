8aa43af6798ec408c2987151062e97db80822859
iurov <34243888+iurov@users.noreply.github.com>
2017-12-19 02:00:10 +1100

    список студентов группы для одмена (не доделано)

---
+++ group_students_admin.php
@@ -0,0 +1,73 @@
+<?php include('search.php');?>
+
+<body>
+	<!--Меню-->
+	<?php include('menu.php');?>
+	<!--Контент-->	
+	<div class="row" style="background:#D4D4D3;">
+		<!--Меню-->
+		
+		<?php
+		$idg = $_GET['id'];
+		$i = 1;
+		$stmt = $db->query('SELECT student.studygroup as idgr, person.lastname as lastname, person.name as name, person.fathername as fathername, groups.studygroup as studygroup, pterodactyl.name as place, vacancy.about as vacancy
+			FROM student
+			left join person on student.id = person.id_person
+			left join groups on student.studygroup = groups.id_group
+			left join request on student.id = request.id_stud
+			left join vacancy on request.id_vac = vacancy.id_vac
+			left join pterodactyl on vacancy.id_pter = pterodactyl.id
+		');
+		
+		echo
+		'
+		<div class="column small-12 medium-12 large-12" style="padding-top:10px;">
+		<div style="background:white">
+		<div class="block-absolute">
+			<p2 style="font-size:xx-large">Прикладная информатика группа Б8419А</p2>
+			<table>
+				<thead>
+					<tr  onclick="window.location.href="group_students.php"; return false>
+						<th>№</th>
+						<th>Фамилия</th>
+						<th>Имя</th>
+						<th>Отчество</th>
+						<th>Место прохождения практики</th>
+					</tr>
+				</thead>
+				<tbody>';
+				while ($row = $stmt->fetch())
+				{
+				$idgr = $row['idgr'];
+				$studygroup = $row['studygroup'];
+				$lastname = $row['lastname'];
+				$name = $row['name'];
+				$fathername = $row['fathername'];
+				$place = $row['place'];
+				$vacancy = $row['vacancy'];
+				if ($idg == $idgr){
+					echo'
+					<tr>
+						<td>'.$i.'</td>
+						<td>'.$lastname.'</td>
+						<td>'.$name.'</td>
+						<td>'.$fathername.'</td>
+						<td>'.'Компания: '.$place. '<br>'.' Вакансия: '.$vacancy.'</td>
+					</tr>';
+					$i = $i +1;};
+					};
+					echo'
+				</tbody>
+			</table>
+		</div></div>
+			
+		
+		';
+		if($role=="RMAD"){
+			echo'<button type="submit">Выбрать</button>';
+		}
+		?>
+		
+	</div>
+</body>	
+	
