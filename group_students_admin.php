b92d6f0276b3bd00dc00b9e111b875b7458ad9b7
iurov <34243888+iurov@users.noreply.github.com>
2017-12-19 16:24:56 +1000

    Add files via upload

--- group_students_admin.php
+++ group_students_admin.php
@@ -10,7 +10,7 @@
 		<?php
 		$idg = $_GET['id'];
 		$i = 1;
-		$stmt = $db->query('SELECT student.studygroup as idgr, person.lastname as lastname, person.name as name, person.fathername as fathername, groups.studygroup as studygroup, pterodactyl.name as place, vacancy.about as vacancy
+		$stmt = $db->query('SELECT student.studygroup as idgr, person.lastname as lastname, person.name as name, person.fathername as fathername, groups.studygroup as studygroup, pterodactyl.name as place, vacancy.about as vacancy, groups.speciality as speciality
 			FROM student
 			left join person on student.id = person.id_person
 			left join groups on student.studygroup = groups.id_group
@@ -23,8 +23,24 @@
 		'
 		<div class="column small-12 medium-12 large-12" style="padding-top:10px;">
 		<div style="background:white">
-		<div class="block-absolute">
-			<p2 style="font-size:xx-large">Прикладная информатика группа Б8419А</p2>
+		<div class="block-absolute">';
+		while ($row = $stmt->fetch())
+			{
+			$idgr = $row['idgr'];
+			if ($idg == $idgr){
+			$studygroup = $row['studygroup'];
+			$speciality = $row['speciality'];};}	
+			echo'
+			<p2 style="font-size:xx-large">'.$speciality.' '.$studygroup.'</p2>';
+			$stmt = $db->query('SELECT student.studygroup as idgr, person.lastname as lastname, person.name as name, person.fathername as fathername, groups.studygroup as studygroup, pterodactyl.name as place, vacancy.about as vacancy, groups.speciality as speciality
+			FROM student
+			left join person on student.id = person.id_person
+			left join groups on student.studygroup = groups.id_group
+			left join request on student.id = request.id_stud
+			left join vacancy on request.id_vac = vacancy.id_vac
+			left join pterodactyl on vacancy.id_pter = pterodactyl.id
+		');
+			echo'
 			<table>
 				<thead>
 					<tr  onclick="window.location.href="group_students.php"; return false>
