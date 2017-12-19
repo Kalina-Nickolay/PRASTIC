45aa59f2306a0d4a9c30ff325932f329a8c18c6d
AlyaMarkova <alya.markova.3@gmail.com>
2017-12-19 12:26:19 +1000

    лого на общей стр вакансий

--- vacancy.php
+++ vacancy.php
@@ -5,10 +5,9 @@
 	<?php include('menu.php');?>
 	<!--Контент-->	
 	<div class="row" style="background:#D4D4D3;">
-		<!--Меню-->
 		
 		<?php
-		$stmt = $db->query('SELECT vacancy.id_vac as id_vac, vacancy.about as ab, pterodactyl.name as name, vacancy.practic as practic, vacancy.privet as privet, vacancy.start as start, vacancy.finish as finish
+		$stmt = $db->query('SELECT vacancy.id_vac as id_vac, vacancy.id_pter as id_pter, vacancy.about as ab, pterodactyl.name as name, vacancy.practic as practic, vacancy.privet as privet, vacancy.start as start, vacancy.finish as finish, vacancy.logo as logo
 			FROM vacancy 
 			inner JOIN pterodactyl ON vacancy.id_pter = pterodactyl.id
 		');
@@ -24,39 +23,30 @@
 		$student_dities=$row['practic'];//Обязанности:
 		$student_welcome=$row['privet'];//Приветствуется:
 		
-		echo
-		'
-		<a class="column small-6 medium-6 large-6" href="vacancy_detail.php?id='.$id_vac.'">
-		<div class="row" id="trunk">
-		<div class="column small-4 medium-4 large-4" style="   display: block; padding:10px;">
-			<br>
-		<p4>от '.$first_date.'</p4>
-			<br>
-		<p4>до '.$last_date.'</p4>
-		
-		<br>
-		<img src="images/7026.jpg"></img>
-		<br>
-		</div>
-		<div class="column small-8 medium-8 large-8" style="padding:10px;">
-		<p1>'.$name_vacancy.'</p1>
-		<hr style="border: none; background-color: #EF9C00; color: #EF9C00; height: 3px;  padding:0; margin:0; margin-top:-5px; margin-bottom:7px;">
-		<p2>'.$name_company.'</p2>
-		<br>
-		<p3>Обязанности: </p3>
-		<br><p3>'.$student_dities.'</p3>
-		<br>
-		<p3>Приветствуется:</p3>
-		<br><p3>'.$student_welcome.'</p3>
-		</div>
-		</div>
-		</a>
-		';
-		
-		
-		
-		}
 		?>
+		<a class="column small-6 medium-6 large-6" href="vacancy_detail.php?id=<? echo $id_vac ?>">
+			<div class="row" id="trunk" style="height:207px;">
+				<div class="column small-4 medium-4 large-4" style="   display: block; padding:10px;"><br>
+					<p4>от <? echo $first_date ?></p4><br>
+					<p4>до <? echo $last_date ?></p4><br>
+					<? if ($row['logo'] != '') {
+						?> <img src="files/logo/<? echo $row['id_pter'] ?>/<? echo $row['logo']?>"></img><br> <?
+					} ?>
+					
+				</div>
+				<div class="column small-8 medium-8 large-8" style="padding:10px;">
+					<p1><? echo $name_vacancy ?></p1>
+					<hr style="border: none; background-color: #EF9C00; color: #EF9C00; height: 3px;  padding:0; margin:0; margin-top:-5px; margin-bottom:7px;">
+					<p2><? echo $name_company ?></p2>
+					<br>
+					<p3>Обязанности: </p3>
+					<br><p3><? echo $student_dities ?></p3>
+					<br>
+					<p3>Приветствуется:</p3>
+					<br><p3><? echo $student_welcome ?></p3>
+				</div>
+			</div>
+		</a>
+		<? } ?>
 	</div>
-</body>	
-	
+</body>		
