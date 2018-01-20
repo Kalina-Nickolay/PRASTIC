74596a2a077ab1fdc35a7b0854358ddacade9cf0
Kalina-Nickolay <kalina.moran@gmail.com>
2018-01-20 20:13:49 +1000

    а вот бдшэчку то я забыл
    И файлы логотипов и договоров, если вы тестить будете, вам тоже пригодятся.

--- index.php
+++ index.php
@@ -188,7 +188,7 @@
 <script type="text/javascript">
 $(function(){
   $('.slider').glide({
-    autoplay: 3500,
+    autoplay: 2500,
     hoverpause: true, // set to false for nonstop rotate
     arrowRightText: '&rarr;',
     arrowLeftText: '&larr;'
